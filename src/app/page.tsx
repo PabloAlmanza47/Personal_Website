7c8ca67bdb83973ae646e1a58186fbc01ca89d67
Mistical47 <pabloalmanza3247@gmail.com>
2026-01-27 00:27:14 -0600

    Start to personal website, adding a simple calling card and information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,100 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope, FaJava} from 'react-icons/fa';
+import { SiJavascript, SiPython, SiReact, SiTypescript, SiHtml5, SiCss3, SiNodedotjs, SiNextdotjs, SiCplusplus, SiHaskell, SiTailwindcss, SiGit} from "react-icons/si";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="bg-[#113532] font-bold flex justify-center items-center h-screen">
+        <div className="bg-white/20 p-2 outline outline-white/10 backdrop-blur-md w-125 h-60 relative rounded-3xl"> 
+          <div className="bg-gray-950 w-full h-full flex flex-row rounded-2xl p-2 outline outline-white/15">
+            {/* Personal information */}
+            <div className="w-1/2 flex flex-col justify-center">
+              {/* Top part */}
+              <div className="flex justify-center h-full">
+                <Image
+                  src="/Pablo.jpg"
+                  alt="me"
+                  width={100}
+                  height={100}
+                  className="rounded-full object-cover"
+                />
+              </div>
+              {/* Bottom Part */}
+              <div className="flex flex-col items-center h-full ">
+                <h1 className="flex flex-1 text-sm font-thin font-serif justify-center items-center">
+                  Contact information
+                </h1>
+                <div className="bg-white/10 rounded-xl outline outline-white/15 flex flex-1 text-xs gap-4 justify-center items-center w-35">
+                  <a 
+                    href="https://github.com/PabloAlmanza47"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white hover:text-gray-950 transition"
+                  >
+                    <FaGithub size={18} />
+                  </a>
+                  <a 
+                    href="https://www.linkedin.com/in/pabloalmanza/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white hover:text-gray-950 transition"
+                  >
+                    <FaLinkedin size={18} />
+                  </a>
+                  <a 
+                    href="https://www.instagram.com/mindsetofpablo/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white hover:text-gray-950 transition"
+                  >
+                    <FaInstagram size={18} />
+                  </a>
+                  <a 
+                    href="mailto:pabloalmanza3247@gmail.com" 
+                    className="text-white hover:text-gray-950 transition"
+                  >
+                    <FaEnvelope size={18} />
+                  </a>
+                    
+                </div>
+              </div>
+            </div>
+            {/* Skills */}
+            <div className="w-1/2 flex flex-col gap-2">
+              <div className="bg-white/10 rounded-xl outline outline-white/15 h-2/3 p-2">
+                <p className="text-xs font-thin font-serif">
+                  Computer science student and web developer focused on building practical, user-centered solutions. I learn by doing, solving technical problems, and improving systems, driven by growth, responsibility, and community.                
+                </p>
+              </div>
+              <div className="bg-white/10 rounded-xl outline outline-white/15 h-1/3 p-2 flex flex-col">
+                <h2 className="font-bold text-xs">
+                  Languages
+                </h2>
+                <div className="flex flex-row gap-1">
+                  <SiPython size={13} />
+                  <SiCplusplus size={13} />
+                  <FaJava size={13} />
+                  <SiTypescript size={13} />
+                  <SiJavascript size={13} />
+                  <SiHaskell size={13} />
+                </div>
+                <h2 className="font-bold text-xs">
+                  Tools/Frameworks
+                </h2>
+                <div className="flex flex-row gap-1">
+                  <SiHtml5 size={13} />
+                  <SiCss3 size={13} />
+                  <SiTailwindcss size={13} />
+                  <SiReact size={13} />
+                  <SiNodedotjs size={13} />
+                  <SiNextdotjs size={13} />
+                  <SiGit size={13} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    </main>
   );
 }
